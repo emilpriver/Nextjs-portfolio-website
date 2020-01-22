208e29d3d5a642b025145b5cc962d4ebaecd7597
emilpriver <hello@emilpriver.com>
2020-01-22 23:12:38 +0100

    Worked on scroll to next event

--- pages/index.js
+++ pages/index.js
@@ -29,15 +29,18 @@ class Home extends React.Component {
       this.carousell = document.querySelector(".frontpage");
       this.carousellInner = document.querySelector(".frontpage-carousell");
       this.items = [
-        ...this.carousell.querySelectorAll(".frontpage-carousell-block")
+        ...this.carousell.querySelectorAll(".frontpage-carousell-block-item")
       ];
       this.totalSteps = this.items.length + 2;
       this.totalStepsWithoutLast = this.items.length + 1;
     }
 
     this.step = 0;
+    this.isMoving = false;
 
     this.projectTextRef = React.createRef();
+    this.hero = React.createRef();
+    this.lastElement = React.createRef();
   }
 
   componentDidMount() {
@@ -63,10 +66,15 @@ class Home extends React.Component {
   setBounds() {
     this.carousell.style.height = window.screen.height;
 
-    this.items.forEach((slide, index) => {
+    this.items.forEach(slide => {
       slide.style.height = `${window.screen.height}px`;
       slide.style.top = `${window.screen.height}px`;
     });
+
+    this.hero.current.style.height = `${window.screen.height}px`;
+    this.hero.current.style.top = 0;
+    this.lastElement.current.style.height = `${window.screen.height}px`;
+    this.lastElement.current.style.top = `${window.screen.height * 2}px`;
   }
 
   fadeLetters = (el, type) => {
@@ -87,7 +95,7 @@ class Home extends React.Component {
     });
   };
 
-  fadeImages = (el, type) => {
+  fadeImage = (el, type) => {
     return new Promise(resolve => {
       anime({
         targets: el.querySelectorAll(".image"),
@@ -109,16 +117,55 @@ class Home extends React.Component {
     ["handleMovementBlocks"].forEach(fn => (this[fn] = this[fn].bind(this)));
   }
 
-  handleMovementBlocks(event) {
-    if (this.step === 0 && event.deltaY < 0) {
-      this.carousellInner.style.transform = `translate3d(0, ${window.screen.height}px, 0)`;
-      this.step = 1;
-    } else if (this.step === this.items.length + 2 && event.deltaY) {
-      this.carousellInner.style.transform = `translate3d(0, ${this
-        .totalStepsWithoutLast * window.screen.height}px, 0)`;
-      this.step = this.totalSteps;
-    } else {
+  async handleMovementBlocks(event) {
+    if (!this.isMoving) {
+      this.isMoving = true;
+
+      /**
+       * If scroll is at the top of the page
+       */
+      if (this.step === 0 && event.deltaY < 0) {
+        this.carousellInner.style.transform = `translate3d(0, ${-window.screen
+          .height}px, 0)`;
+        this.step = 1;
+
+        /**
+         * If scroll is at the bottom
+         */
+      } else if (this.step === this.items.length + 2 && event.deltaY > 0) {
+        this.carousellInner.style.transform = `translate3d(0, ${2 *
+          -window.screen.height}px, 0)`;
+        this.step = this.totalSteps;
+      } else {
+        /**
+         * if scroll is at a project
+         */
+        if (this.step - 1 === this.items.length && event.deltaY < 0) {
+          this.carousellInner.style.transform = `translate3d(0, ${2 *
+            -window.screen.height}px, 0)`;
+          this.step = this.totalSteps;
+        }
+
+        if (this.step === 1 && event.deltaY > 0) {
+          this.carousellInner.style.transform = "translate3d(0, 0px, 0)";
+          this.step = 1;
+        }
+
+        const oldElement = this.items[this.step - 1];
+        if (this.step === 2) {
+          await this.fadeLetters(oldElement, "in");
+          await this.fadeImage(oldElement, "out");
+        }
+        this.step += 1;
+        const element = this.items[this.step - 1];
+        if (this.step === 2) {
+          await this.fadeLetters(element, "in");
+          await this.fadeImage(element, "out");
+        }
+      }
+      console.log(this.step);
     }
+    this.isMoving = false;
   }
 
   addEvents() {
@@ -149,7 +196,11 @@ class Home extends React.Component {
         <Nav />
         <div className="frontpage">
           <div className="frontpage-carousell">
-            <div className="frontpage-carousell-block" id="hero">
+            <div
+              className="frontpage-carousell-block"
+              ref={this.hero}
+              id="hero"
+            >
               <div className="container wrapper">
                 <h1>
                   <span className="anime-words">I </span>
@@ -178,7 +229,7 @@ class Home extends React.Component {
             </div>
             {projects.map(item => (
               <div
-                className="project frontpage-carousell-block"
+                className="project frontpage-carousell-block frontpage-carousell-block-item"
                 key={item.acf.slug}
               >
                 <div className="project_wrapper">
@@ -188,6 +239,37 @@ class Home extends React.Component {
                 </div>
               </div>
             ))}
+            <div
+              className="frontpage-carousell-block"
+              ref={this.lastElement}
+              id="hero"
+            >
+              <div className="container wrapper">
+                <h1>
+                  <span className="anime-words">I </span>
+                  <span className="anime-words">am</span>
+                  <span className="anime-words">Emil</span>
+                  <span className="anime-words">Priver.</span>
+                  <br />
+                  <span className="anime-words">A</span>
+                  <span className="anime-words">developer</span>
+                  <span className="anime-words">based</span>
+                  <span className="anime-words">in</span>
+                  <span className="anime-words">Borås,</span>
+                  <span className="anime-words">Sweden</span>
+                </h1>
+                <h3>
+                  <span className="anime-words">Currently</span>
+                  <div className="anime-words">working</div>
+                  <div className="anime-words">at</div>
+                  <div className="anime-words">Rivercode,</div>
+                  <br />
+                  <span className="anime-words">Creating</span>
+                  <div className="anime-words">digital</div>
+                  <div className="anime-words">experiencies.</div>
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
       </>
