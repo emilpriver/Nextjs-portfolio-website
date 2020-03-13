728b55116655880f1854e8b048bbb231e01f0188
emilpriver <emil@priver.dev>
2020-03-13 21:27:03 +0100

    added more ssg

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -18,76 +18,93 @@ function imageURL(source) {
   return imageUrlBuilder(client).image(source);
 }
 
+const allQuery = groq`*[_type == "post"]{
+  slug
+}`;
 const query = groq`*[_type == "post" && slug.current == $slug][0]`;
 
-class SingleArticle extends React.Component {
-  static async getInitialProps(context) {
-    const { slug } = context.query;
-    const post = await client.fetch(query, { slug });
-    return { post };
-  }
+export async function getStaticPaths() {
+  const data = await client.fetch(allQuery).then(response => {
+    return response.map(el => {
+      return {
+        params: {
+          slug: el.slug.current
+        }
+      };
+    });
+  });
 
-  render() {
-    const { post } = this.props;
-
-    if (!post) {
-      return <Error />;
-    }
-
-    const seotitle = post.seo.seo_title
-      ? post.seo.seo_title
-      : `Post: ${post.title}`;
-
-    return (
-      <Layout>
-        <Head
-          title={`Emil Privér - Post: ${post.title}`}
-          seoTitle={`Emil Privér - ${seotitle}`}
-          description={`${post.seo.meta_description}`}
-          ogImage={imageURL(post.thumbnail.asset)
-            .auto("format")
-            .url()}
-        />
-        <Nav />
-        <section id="single-article">
-          <div className="container mx-auto">
-            <div className="date">
-              {dayjs(post.publishedAt).format("MMMM D, YYYY")}
-            </div>
-            <h1 className="title">{post.title}</h1>
-            <div className="tags">
-              {post.categories.map((el, index) => {
-                return (
-                  <span key={el.title}>
-                    {`${el.title}
-                    ${post.categories.length !== index + 1 ? ", " : ""}`}
-                  </span>
-                );
-              })}
-            </div>
-            <div className="thumbnail">
-              <LazyLoad offset={200}>
-                <img
-                  src={imageURL(post.thumbnail.asset)
-                    .auto("format")
-                    .url()}
-                  alt={post.title}
-                />
-              </LazyLoad>
-            </div>
-            <div className="content">
-              <BlockContent
-                serializers={serializer}
-                blocks={post.body}
-                {...client.config()}
-              />
-            </div>
-          </div>
-        </section>
-        <Footer />
-      </Layout>
-    );
-  }
+  return {
+    paths: data,
+    fallback: false
+  };
 }
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const data = await client.fetch(query, { slug });
+  return { props: { post: data } };
+}
+
+const SingleArticle = props => {
+  const { post } = props;
+
+  if (!post) {
+    return <Error />;
+  }
+
+  const seotitle = post.seo.seo_title
+    ? post.seo.seo_title
+    : `Post: ${post.title}`;
+  return (
+    <Layout>
+      <Head
+        title={`Emil Privér - Post: ${post.title}`}
+        seoTitle={`Emil Privér - ${seotitle}`}
+        description={`${post.seo.meta_description}`}
+        ogImage={imageURL(post.thumbnail.asset)
+          .auto("format")
+          .url()}
+      />
+      <Nav />
+      <section id="single-article">
+        <div className="container mx-auto">
+          <div className="date">
+            {dayjs(post.publishedAt).format("MMMM D, YYYY")}
+          </div>
+          <h1 className="title">{post.title}</h1>
+          <div className="tags">
+            {post.categories.map((el, index) => {
+              return (
+                <span key={el.title}>
+                  {`${el.title}
+                    ${post.categories.length !== index + 1 ? ", " : ""}`}
+                </span>
+              );
+            })}
+          </div>
+          <div className="thumbnail">
+            <LazyLoad offset={200}>
+              <img
+                src={imageURL(post.thumbnail.asset)
+                  .auto("format")
+                  .url()}
+                alt={post.title}
+              />
+            </LazyLoad>
+          </div>
+          <div className="content">
+            <BlockContent
+              serializers={serializer}
+              blocks={post.body}
+              {...client.config()}
+            />
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </Layout>
+  );
+};
 
 export default SingleArticle;
--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -19,80 +19,78 @@ const query = groq`*[_type == "post"] | order(publishedAt desc) {
   categories,
 }`;
 
-class Articles extends React.Component {
-  static async getInitialProps() {
-    const posts = await client.fetch(query);
-    return { posts };
-  }
+export async function getStaticProps() {
+  const data = await client.fetch(query);
+  return { props: { posts: data } };
+}
 
-  render() {
-    const { posts } = this.props;
-    return (
-      <Layout>
-        <Head
-          title="Emil Privér - Posts"
-          description="Emil Privér - Posts written by Emil Privér"
-        />
-        <Nav />
-        <section id="articles">
-          <div className="container mx-auto">
-            <h1>Posts</h1>
-            {posts ? (
-              <div className="row">
-                {posts.map(el => {
-                  return (
-                    <div
-                      className="article w-full md:w-1/2 lg:w-1/3 float-left"
-                      key={el.title}
-                    >
-                      <div className="wrapper">
+const Posts = props => {
+  const { posts } = props;
+  return (
+    <Layout>
+      <Head
+        title="Emil Privér - Posts"
+        description="Emil Privér - Posts written by Emil Privér"
+      />
+      <Nav />
+      <section id="articles">
+        <div className="container mx-auto">
+          <h1>Posts</h1>
+          {posts ? (
+            <div className="row">
+              {posts.map(el => {
+                return (
+                  <div
+                    className="article w-full md:w-1/2 lg:w-1/3 float-left"
+                    key={el.title}
+                  >
+                    <div className="wrapper">
+                      <Link
+                        href="/posts/[slug]"
+                        as={`/posts/${el.slug.current}`}
+                      >
+                        <a>
+                          <h3>{el.title}</h3>
+                        </a>
+                      </Link>
+                      <span className="date">
+                        {dayjs(el.publishedAt).from()}
+                      </span>
+                      <div className="tags">
+                        {el.categories.map((item, index) => {
+                          return (
+                            <span key={item.title}>
+                              {`${item.title}
+                                ${
+                                  el.categories.length !== index + 1 ? ", " : ""
+                                }`}
+                            </span>
+                          );
+                        })}
+                      </div>
+                      <div className="read">
                         <Link
                           href="/posts/[slug]"
                           as={`/posts/${el.slug.current}`}
                         >
-                          <a>
-                            <h3>{el.title}</h3>
-                          </a>
+                          <a>Read posts -></a>
                         </Link>
-                        <span className="date">
-                          {dayjs(el.publishedAt).from()}
-                        </span>
-                        <div className="tags">
-                          {el.categories.map((item, index) => {
-                            return (
-                              <span key={item.title}>
-                                {`${item.title}
-                                ${
-                                  el.categories.length !== index + 1 ? ", " : ""
-                                }`}
-                              </span>
-                            );
-                          })}
-                        </div>
-                        <div className="read">
-                          <Link
-                            href="/posts/[slug]"
-                            as={`/posts/${el.slug.current}`}
-                          >
-                            <a>Read posts -></a>
-                          </Link>
-                        </div>
                       </div>
                     </div>
-                  );
-                })}
-              </div>
-            ) : (
-              <div className="row">
-                <span>Error loading posts, please reload</span>
-              </div>
-            )}
-          </div>
-        </section>
-        <Footer />
-      </Layout>
-    );
-  }
-}
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="row">
+              <span>Error loading posts, please reload</span>
+            </div>
+          )}
+        </div>
+      </section>
+      <Footer />
+    </Layout>
+  );
+};
 
-export default Articles;
+export default Posts;
